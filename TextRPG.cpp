ded8d268ebb43613e24e6090e3dd6fbbcb18f044
Oreos <daltonoreo@live.com>
2018-07-02 22:30:51 -0500

    Added semi-colen after Character Class, Added Acessors and Modifers to Mana and Health in Character Class

--- TextRPG.cpp
+++ TextRPG.cpp
@@ -8,16 +8,34 @@ using namespace std;
 
 class Character
 {
+private:
+	//Variables
 	int health;
 	int mana;
-}
+public:
+	//Acessors
+	int GetHealth() { return health; }
+	int GetMana() { return mana; }
+	//Modifers
+	void SetHealth(int x)
+	{
+		this->health = x;
+	}
+	void SetMana(int x)
+	{
+		this->mana = x;
+	}
+};
 //Functions
 
 void PrintMainMenu()
 {
-	cout << "Health: " << " Mana: " << endl;
+	//Class Caller
+	Character PC; //'Player Character'
+
+	cout << "Health: " << PC.GetHealth() << " Mana: " << PC.GetMana() << endl;
 	cout << "---------------------" << endl;
-	cout << "1.Option" << endl;
+	cout << "1.Travel" << endl;
 	cout << "2.Rest" << endl;
 	cout << "3.Inventory" << endl;
 	cout << "4.Character Sheet" << endl;
@@ -40,11 +58,11 @@ void PrintMainMenu()
 int main()
 {
 	//Class Callers
-
-	//Variables
+	Character PC; //'Player Character'
+				  //Variables
 	bool Playing = true; //Keeps Gamerunning
 	int Choice; //Menu Choices
-	//Gameloop
+				//Gameloop
 	while (Playing)
 	{
 		PrintMainMenu();
