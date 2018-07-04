13a9e87c32f261f14571e3fe12bb5f6277ff7163
Oreos <daltonoreo@live.com>
2018-07-04 00:29:05 -0500

    Fixed Class Bug Where Private Values Didn't Update

--- TextRPG.cpp
+++ TextRPG.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
-
 #include <cstdlib>
-
 #include <ctime>
 
 using namespace std;
@@ -16,31 +14,19 @@ class Character
 
 {
 private:
-
-
 	//Variables
-
 	int v_Health;
-
 	int v_Mana;
-
 	int v_MaxHealth;
-
 	int v_MaxMana;
 
 public:
-
 	//Acessors
-
 	int GetMaxHealth() { return v_MaxHealth; } //Returns Max Health Integar
-
 	int GetMaxMana() { return v_MaxMana; } //Returns Max Mana Integar
-
 	int GetHealth() { return v_Health; } //Returns Health Integar
-
 	int GetMana() { return v_Mana; } //Returns Mana Integar
-
-									 //Modifers
+//Modifers
 
 	void SetHealth(int Health) {
 		v_Health = Health;
@@ -67,43 +53,27 @@ public:
 
 
 		std::cout << "Health: " << GetHealth() << "/" << GetMaxHealth() << " Mana: " << GetMana() << "/" << GetMaxMana() << endl;
-
 		std::cout << "---------------------" << endl;
-
 		std::cout << "1.Travel" << endl;
-
 		std::cout << "2.Rest" << endl;
-
 		std::cout << "3.Inventory" << endl;
-
 		std::cout << "4.Character Sheet" << endl;
-
 		std::cout << "5.Save Character" << endl;
-
 		std::cout << "6.Load Character" << endl;
-
 		std::cout << "7.Quit" << endl;
-
 		std::cout << "---------------------" << endl;
 	}
-	//Changes Max Mana Value
-
 	//Character Intilizer
-
 	Character()
 	{
-
 		v_MaxMana = 10;//10 is a place holder
-
 		v_MaxHealth = 10; //10 is a place holder
-
 		v_Health = 0; //0 is a place holder
-
 		v_Mana = 0; //0 is a place holder
 	}
 };
 
-//Function
+//Functions
 
 //Empty Space
 
@@ -122,7 +92,7 @@ int main()
 
   while (Playing) //Gameloop
   {	
-    PrintMainMenu();
+    PC.PrintMainMenu();
 		
     cin >> Choice; //Do Not Enter Characters
 		
@@ -134,26 +104,17 @@ int main()
 		
       case 2: 			
       system("CLS");
-
-<<<<<<< HEAD
-
-
-//Empty Space
-=======
       std::cout << "Warning!: Resting Outside Of A Inn Will Will Drain Your Hunger!" << endl;
       std::cout << "------------------------------" << endl;
       std::cout << "1.Rest" << endl;
       std::cout << "2.Back" << endl;
       std::cout << "------------------------------" << endl;
-      cin >> Choice;
-			
+      cin >> Choice;		
       switch (Choice)
 	    {
-        case 1: //The Modifers PC.SetHealth(), PC.SetMana(), PC.SetMaxHealth(), and PC,SetMaxMana() Are Not Working Can you Fix Them For Me?
-        ChangingVariable = rand() % PC.GetMaxHealth() + PC.GetHealth();
-        ChangingVariable2 = rand() % PC.GetMaxMana() + PC.GetMana();
-        cout << ChangingVariable << endl;
-        cout << ChangingVariable2 << endl;
+        case 1:
+        ChangingVariable = rand() % (PC.GetMaxHealth()/2) + PC.GetHealth();
+        ChangingVariable2 = rand() % (PC.GetMaxMana()/2) + PC.GetMana();
         PC.SetHealth(ChangingVariable);
         PC.SetMana(ChangingVariable2);
         if (PC.GetHealth() > PC.GetMaxHealth())
@@ -177,8 +138,6 @@ int main()
       case 3:			
       system("CLS");			
       break;
->>>>>>> f116bd7b53c2933378d24d07e5a1fd97b3fbe9df
-
       case 4:
       system("CLS");
       break;
@@ -191,153 +150,6 @@ int main()
       system("CLS");
       break;
 
-<<<<<<< HEAD
-
-int main()
-{
-
-	//Class Callers
-
-	Character PC; //'Player Character'
-
-	srand(time(NULL)); //Randomizer
-
-					   //Variables
-
-	bool Playing = true; //Keeps Gamerunning
-
-	int Choice; //Menu Choices
-
-	int ChangingVariable; //Throw Away Variable
-
-	int ChangingVariable2; //Throw Away Variable
-
-	while (Playing) //Gameloop
-
-	{
-
-		PC.PrintMainMenu();
-
-		cin >> Choice; //Do Not Enter Characters
-
-		switch (Choice)
-		{
-
-		case 1:
-
-			system("CLS");
-
-			break;
-
-		case 2:
-
-			system("CLS");
-
-			std::cout << "Warning!: Resting Outside Of A Inn Will Will Drain Your Hunger!" << endl;
-
-			std::cout << "------------------------------" << endl;
-
-			std::cout << "1.Rest" << endl;
-
-			std::cout << "2.Back" << endl;
-
-			std::cout << "------------------------------" << endl;
-
-			cin >> Choice;
-
-			switch (Choice)
-			{
-
-			case 1: //The Modifers PC.SetHealth(), PC.SetMana(), PC.SetMaxHealth(), and PC,SetMaxMana() Are Not Working Can you Fix Them For Me?
-
-				ChangingVariable = rand() % (PC.GetMaxHealth()/2) + PC.GetHealth();
-
-				ChangingVariable2 = rand() % (PC.GetMaxMana()/2) + PC.GetMana();
-
-				cout << ChangingVariable << endl;
-
-				cout << ChangingVariable2 << endl;
-
-				PC.SetHealth(ChangingVariable);
-
-				PC.SetMana(ChangingVariable2);
-
-				if (PC.GetHealth() > PC.GetMaxHealth())
-
-				{
-
-					PC.SetHealth(PC.GetMaxHealth());
-
-				}
-
-				if (PC.GetMana() > PC.GetMaxMana())
-
-				{
-
-					PC.SetMana(PC.GetHealth());
-
-				}
-
-				break;
-
-			case 2:
-
-				break;
-
-			default:
-
-				std::cout << "Invalid Choice" << endl;
-
-				break;
-
-			}
-
-		case 3:
-
-			system("CLS");
-
-			break;
-
-		case 4:
-
-			system("CLS");
-
-			break;
-
-		case 5:
-
-			system("CLS");
-
-			break;
-
-		case 6:
-
-			system("CLS");
-
-			break;
-
-		case 7:
-
-			return 0;
-
-			break;
-
-		default:
-
-			system("CLS");
-
-			std::cout << "Invalid Choice" << endl;
-
-			break;
-
-		}
-
-	}
-
-	return 0;
-
-}
-=======
       case 7:
       return 0;
       break;
@@ -351,4 +163,4 @@ int main()
   }
 return 0;
 }
->>>>>>> f116bd7b53c2933378d24d07e5a1fd97b3fbe9df
+
